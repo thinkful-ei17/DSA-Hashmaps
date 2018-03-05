8550327d87e4cd9fd1d3c56368c69c3d058ef1d0
Brent Guistwite <brentguistwite@gmail.com>
2018-03-05 15:50:18 -0500

    workign on palperm function

--- palindrome-permutations.js
+++ palindrome-permutations.js
@@ -3,9 +3,34 @@ const hashMap = require('./hashMap');
 const palPerm = new hashMap();
 
 const palPermFinder = (string) => {
-  string.split('').sort().map((letter) =>{
-    console.log(letter);
+  string.split('').forEach((letter) => {
+    try {
+      palPerm.set(letter, palPerm.get(letter)+1);
+    }
+    catch(err) {
+      console.log('error thrown');
+      palPerm.set(letter, 1);
+    }
   });
 };
 
-palPermFinder('dad');
+palPermFinder('racecar');
+console.log(palPerm);
+
+
+// can never have more than 1 odd number
+// odd number cant be greater than 1
+
+// if num > 1 {
+// return num % 2 === 0;
+//} 
+
+
+
+//a: 1
+//a: 2
+
+// a: 2
+// c: 2
+// e: 1
+// r: 2
